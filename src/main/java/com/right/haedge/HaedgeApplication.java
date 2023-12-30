1704048c00fa450431e8a375832e26e5fb41d8a2
seonghoJoo <etkl8888@naver.com>
2023-12-30 21:12:25 +0900

    Initial commit

---
+++ src/main/java/com/right/haedge/HaedgeApplication.java
@@ -0,0 +1,13 @@
+package com.right.haedge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaedgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaedgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/right/haedge/HaedgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.right.haedge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaedgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
