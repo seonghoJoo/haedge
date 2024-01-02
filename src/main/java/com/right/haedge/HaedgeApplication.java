96fb82bfeffcc1a5a4ea8660ea379867e202e35b
seonghJoo <etkl8888@naver.com>
2024-01-03 00:05:35 +0900

    [FEAT] @EnableJpaRepositories 기능 추가

--- src/main/java/com/right/haedge/HaedgeApplication.java
+++ src/main/java/com/right/haedge/HaedgeApplication.java
@@ -2,8 +2,10 @@ package com.right.haedge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class HaedgeApplication {
 
 	public static void main(String[] args) {
