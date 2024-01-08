a25821cf3a880a347857384fac4dfa52e6ff1d61
seonghJoo <etkl8888@naver.com>
2024-01-08 18:26:44 +0900

    [FEAT] 주식 추천 저장 ServiceLayer

--- src/main/java/com/right/haedge/service/StockServiceImpl.java
+++ src/main/java/com/right/haedge/service/StockServiceImpl.java
@@ -1,15 +1,71 @@
 package com.right.haedge.service;
 
+import com.right.haedge.entity.*;
+import com.right.haedge.repository.*;
+import com.right.haedge.service.dto.StockDTO;
 import com.right.haedge.service.dto.StockSaveDTO;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class StockServiceImpl implements StockService{
 
+    private final VideoRepository videoRepository;
+    private final ChannelRepository channelRepository;
+    private final StockRepository stockRepository;
+    private final RecommendStockRepository recommendStockRepository;
+    private final ReasonRepository reasonRepository;
+
+    /**
+     *  저장 할 것
+     *  channel 없으면 1건
+     *  video 없으면 1건
+     *  stock 없으면 1건
+     *  recommend_stock 다건
+     *  reason 다건
+     * */
     @Override
-    public void saveRecommendStock(StockSaveDTO reqToDto) {
-        log.info("{}", reqToDto);
+    @Transactional
+    public void saveRecommendStock(StockSaveDTO saveDto) {
+
+        Channel channel = channelRepository.findByChannelHashed(saveDto.getChannelId())
+                .orElseGet(() -> channelRepository.save(Channel.builder()
+                                .name(saveDto.getChannelName())
+                                .channelHashed(saveDto.getChannelId())
+                        .build()));
+
+        Video video = videoRepository.findByVideoHashed(saveDto.getVideoId())
+                .orElseGet(() -> videoRepository.save(Video.builder()
+                        .channel(channel)
+                        .thumbnail(saveDto.getThumbnailUrl())
+                        .videoHashed(saveDto.getVideoId())
+                        .build()));
+
+        for (StockDTO stockDTO : saveDto.getStocks()) {
+            Stock stock = stockRepository.findByName(stockDTO.getName()).orElseGet(() -> stockRepository.save(
+                    Stock.builder()
+                            .name(stockDTO.getName())
+                            .ticker(stockDTO.getTicker())
+                            .build()));
+            RecommendStock recommendStock = RecommendStock.builder()
+                    .stock(stock)
+                    .video(video)
+                    .weight(stockDTO.getWeight())
+                    .rgstDtm(LocalDateTime.now())
+                    .build();
+            recommendStockRepository.save(recommendStock);
+            List<Reason> reasons = stockDTO.getReason().stream()
+                    .map(s -> new Reason(s, recommendStock))
+                    .collect(Collectors.toList());
+            reasonRepository.saveAll(reasons);
+        }
     }
 }
